fccf5b2d2fa1ef4ca7fa0a7607d76f0cd9e45a17
Jason Lally <jason@placematters.org>
2015-03-25 21:57:17 +0000

    set up form and validations on academy page

--- js/datasf.js
+++ js/datasf.js
@@ -8,6 +8,32 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Set up custom validators to check for gov domains
+  jQuery.validator.addMethod("matchGov", function(value, element) {
+    return this.optional(element) || /[_a-z0-9-]+(\.[_a-z0-9-]+)*@(sfgov.org|sfmta.com|sfwater.org|sfmta.org|sfdph.org|sfport.com|flysfo.com|sfdpw.org)/.test(value);
+  }, "Please enter a valid SFGov email address");
+  
+  //Reset validator defaults to integrate with Bootstrap 3 conventions
+  $.validator.setDefaults({
+    highlight: function(element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
+
   $('a.page-scroll').bind('click', function(event) {
     var $anchor = $(this);
     var $bodyMargin = $('body').css('margin-top').split("px")[0];
@@ -25,8 +51,11 @@ $(function() {
 
   var $container = $('.isotope').isotope({
     itemSelector: '.course-item',
-    layoutMode: 'fitRows'
+    masonry: {},
+    filter: "*"
   });
+
+
   // filter functions
   var filterFns = {
     // show if number is greater than 50
@@ -50,16 +79,38 @@ $(function() {
     });
   });
   // change is-checked class on buttons
-  $('.button-group').each(function(i, buttonGroup) {
+  $('.btn-group').each(function(i, buttonGroup) {
     var $buttonGroup = $(buttonGroup);
     $buttonGroup.on('click', 'button', function() {
       $buttonGroup.find('.is-checked').removeClass('is-checked');
       $(this).addClass('is-checked');
     });
   });
+
+  $("#form-learn-more").validate({
+    rules: {
+      _replyTo: {
+        required: true,
+        email: true,
+        matchGov: true
+      }
+    },
+    submitHandler: function(form) {
+      var data = $(form).serialize();
+      data += '&_subject=Please add me to the Data Academy list';
+      console.log(data);
+      $.ajax({
+        url: "//formspree.io/jason.lally@sfgov.org",
+        method: "POST",
+        data: data,
+        dataType: "json"
+      })
+      .done(function() {
+        $('#form-learn-more-success').show();
+      });
+    }
+  });
 });
-
-
 
 // Highlight the top nav as scrolling occurs
 /*$('body').scrollspy({
