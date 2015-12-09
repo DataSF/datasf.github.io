4835749ac60385dd175d46a49ba34688bd9e7875
Jason Lally <jason@placematters.org>
2015-12-09 01:25:07 +0000

    update datasf in progress with new measures

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -2,7 +2,7 @@ $(function() {
   //buildPage();
   // Dataset invnentory q6xv-9c3b 
   // Inventory status wuym-j7si
-  var baseIDs = ['wuym-j7si', 'wuym-j7si', 'q6xv-9c3b', 'q6xv-9c3b', 'q6xv-9c3b','q6xv-9c3b'];
+  var baseIDs = ['wuym-j7si', 'wuym-j7si', 'q6xv-9c3b', 'q6xv-9c3b', 'q6xv-9c3b','q6xv-9c3b','q6xv-9c3b','q6xv-9c3b','q6xv-9c3b','q6xv-9c3b','wuym-j7si','q6xv-9c3b','q6xv-9c3b'];
 
   var queryStrings = [
     [
@@ -24,12 +24,39 @@ $(function() {
     ].join('&'), [
       '$select=publishing_status,count(dataset_name)+AS+count',
       '$group=publishing_status',
+      '$where=publishing_status="Published"+OR+publishing_status="Not Published"',
       '$order=count+desc'
     ].join('&'), [
       '$select=category,count(*)+AS+number_of_datasets',
       '$group=category',
       '$order=number_of_datasets+desc',
       '$where=category<>""'
+    ].join('&'), [
+      '$select=CASE(natively_hosted=true,"Natively hosted",natively_hosted=false,"Not natively hosted")+as+natively_hosted_status,count(*)',
+      '$group=natively_hosted_status',
+      '$where=publishing_status="Published"'
+    ].join('&'), [
+      '$select=on_time,count(*)',
+      '$group=on_time',
+      '$where=publishing_status="Published"'  
+    ].join('&'), [
+      '$select=CASE(lag=true,"Data lag",lag=false,"No data lag")+as+lag_status,count(*)',
+      '$group=lag_status',
+      '$where=publishing_status="Published"'
+    ].join('&'), [
+      '$select=CASE(metadata_complete=true,"Complete",metadata_complete=false,"Not complete")+as+metadata_status,count(*)',
+      '$group=metadata_status',
+      '$where=publishing_status="Published"'
+    ].join('&'), [
+      '$select=sum(published_through_december_2015)+as+published,(sum(target_december_2015)-published)+as+not_published',
+    ].join('&'), [
+      '$select=data_classification,sum(CASE(publishing_status="Published",1))%20as%20published,%20sum(CASE(publishing_status="Not%20Published",%201))%20as%20not_published,count(*)%20as%20total',
+      '$group=data_classification',
+      '$order=total+desc'
+    ].join('&'), [
+      '$select=department_priority,sum(CASE(publishing_status="Published",1))%20as%20published,%20sum(CASE(publishing_status="Not%20Published",%201))%20as%20not_published,count(*)%20as%20total',
+      '$group=department_priority',
+      '$order=department_priority+asc'
     ].join('&')
   ];
 
@@ -63,6 +90,44 @@ $(function() {
       x: 'category',
       value: ['number_of_datasets']
     }
+  }, {
+    type: "pie",
+    label: "natively_hosted_status"
+  },
+  {
+    type: "pie",
+    label: "on_time"
+  },
+  {
+    type: "pie",
+    label: "lag_status"
+  },
+  {
+    type: "pie",
+    label: "metadata_status"
+  },
+  {
+    type: "pie"
+  },
+  {
+    type: "bar",
+    keys: {
+      x: 'data_classification',
+      value: ['published','not_published']
+    },
+    groups: [['published','not_published']],
+    transform: 'normalize',
+    yFormat: '.2p'
+  },
+  {
+    type: "bar",
+    keys: {
+      x: 'department_priority',
+      value: ['published','not_published']
+    },
+    groups: [['published','not_published']],
+    transform: 'normalize',
+    yFormat: '.2p'
   }];
   
   processCharts(0,baseIDs,queryStrings,options,[]);
