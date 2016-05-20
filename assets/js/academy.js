981e32f849fc1437da928e0b5071ec4c9f2d8005
Jason Lally <jason.lally@gmail.com>
2016-05-20 21:24:34 +0000

    Updated '/assets/js/academy.js' via CloudCannon

--- assets/js/academy.js
+++ assets/js/academy.js
@@ -1,8 +1,4 @@
 $(function() {
-  $.getJSON('https://www.googleapis.com/calendar/v3/calendars/6o082o4eh3hvfvn2sdgp9h8ebk@group.calendar.google.com/events?key=AIzaSyBlqUEZTgDb5TNNj2rNq4q0u9ZF1hFkkiY', function(data) {
-    console.log(data);
-  });
-  
     //Filter academy list
   var $btns = $('.filter-topic').click(function(ev) {
     ev.preventDefault();
